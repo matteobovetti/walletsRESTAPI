641a5424ba8588ca66dda8ab8fb1947d3c01493b
Matteo Bovetti <matteobovetti@gmail.com>
2015-02-10 17:26:53 +0100

    added JRange. modified update.html layout.

--- public/app/movements/movements.js
+++ public/app/movements/movements.js
@@ -3,52 +3,64 @@
 angular.module('walletsApp.movements', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/movements', {
-    templateUrl: 'movements/movements.html',
-    controller: 'MovementsCtrl'
-  });
+	$routeProvider.when('/movements', {
+		templateUrl: 'movements/movements.html',
+		controller: 'MovementsCtrl'
+	});
 }])
 
 .controller('MovementsCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
-    
-    $scope.movements = [];
-    $scope.query = '';
-    
-    angular.element("#movement-badge").text("0");
-    
-    $scope.getMovemenets = function() {
-        
-        $http.get('/movements').
-        success(function(data, status, headers, config) {
-            $scope.movements = data;
-        }).
-        error(function(data, status, headers, config) {
-            alert('Opssss!!! the server is not ready');
 
-            console.log(data);
-            console.log(status);
-        });
+	$scope.movements = [];
+	$scope.query = '';
+	$scope.year_selected = moment().year();
+	$scope.slider_value = 0;
 
-    };
-    
-    $scope.delete = function(id) {
-        
-        console.log(id);
-        
-        $http.delete('/deleteMovement/' + id).
-        success(function(data, status, headers, config) {
-            console.log('200 OK');
-            $scope.getMovemenets();
-        }).
-        error(function(data, status, headers, config) {
-            alert('Opssss!!! the server is not ready');
+	angular.element("#movement-badge").text("0");
 
-            console.log(data);
-            console.log(status);
-        });
-        
-    };
-    
-    $scope.getMovemenets();
-    
+	angular.element(".single-slider").jRange({
+		from: 1,
+		to: 12,
+		step: 1,
+		scale: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct', 'Nov','Dec'],
+		format: '',
+		width: 300,
+		showLabels: true
+	});
+	
+	$scope.getMovemenets = function() {
+
+		$http.get('/movements').
+		success(function(data, status, headers, config) {
+			$scope.movements = data;
+		}).
+		error(function(data, status, headers, config) {
+			alert('Opssss!!! the server is not ready');
+
+			console.log(data);
+			console.log(status);
+		});
+
+	};
+
+	$scope.delete = function(id) {
+
+		console.log(id);
+
+		$http.delete('/deleteMovement/' + id).
+		success(function(data, status, headers, config) {
+			console.log('200 OK');
+			$scope.getMovemenets();
+		}).
+		error(function(data, status, headers, config) {
+			alert('Opssss!!! the server is not ready');
+
+			console.log(data);
+			console.log(status);
+		});
+
+	};
+
+	$scope.getMovemenets();
+
 }]);
