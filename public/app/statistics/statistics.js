ff419e0eb34e0ece5f9630d9d4b72ef6b45f989d
Matteo Bovetti <matteobovetti@gmail.com>
2015-03-06 15:00:43 +0100

    New yearly in index in statistics section.

--- public/app/statistics/statistics.js
+++ public/app/statistics/statistics.js
@@ -2,55 +2,67 @@
 
 angular.module('walletsApp.statistics', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/statistics', {
-        templateUrl: 'statistics/statistics.html',
-        controller: 'StatisticsCtrl'
-    });
-}])
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/statistics', {
+			templateUrl: 'statistics/statistics.html',
+			controller: 'StatisticsCtrl'
+		});
+	}])
 
-.controller('StatisticsCtrl', ['$scope', '$http', function($scope, $http) {
+	.controller('StatisticsCtrl', ['$scope', '$http', function($scope, $http) {
 
-    $scope.year_selected = moment().year();
-    $scope.month_selected = moment().month() + 1;
-    $scope.total_cost = 0;
-    $scope.total_entrance = 0;
-    $scope.percentage_cost_entrance = 0;
-    $scope.difference_cost_entrance = 0;
+		$scope.year_selected = moment().year();
+		$scope.month_selected = moment().month() + 1;
+		$scope.total_cost = 0;
+		$scope.total_income = 0;
+		$scope.percentage_cost_income = 0;
+		$scope.difference_cost_income = 0;
+		$scope.total_yearly_cost = 0;
+		$scope.total_yearly_income = 0;
 
-    $scope.getStatistics = function() {
+		$scope.getStatistics = function() {
 
-        $scope.total_cost = 0;
-        $scope.total_entrance = 0;
+			$scope.total_cost = 0;
+			$scope.total_income = 0;
+			$scope.total_yearly_cost = 0;
+			$scope.total_yearly_income = 0;
 
-        $http.get('/movements?y=' + $scope.year_selected + '&m=' + $scope.month_selected).
-        success(function(data, status, headers, config) {
+			$http.get('/movements?y=' + $scope.year_selected + '&m=' + $scope.month_selected).
+			success(function(data, status, headers, config) {
 
-            angular.forEach(data, function(value, key) {
+				angular.forEach(data, function(value, key) {
 
-                if (value.amount > 0)
-                    $scope.total_entrance += value.amount;
-                else
-                    $scope.total_cost += (-1)*value.amount;
+					if (value.frequencytype === 'f')
+						if (value.amount > 0)
+							$scope.total_yearly_income += value.amount;
+						else
+							$scope.total_yearly_cost += (-1)*value.amount;
 
-                $scope.percentage_cost_entrance = 0;
-                if ($scope.total_entrance > 0)
-                    $scope.percentage_cost_entrance = ($scope.total_cost / $scope.total_entrance) * 100;
+					else {
+						if (value.amount > 0)
+							$scope.total_income += value.amount;
+						else
+							$scope.total_cost += (-1)*value.amount;
+					}
 
-                $scope.difference_cost_entrance = $scope.total_entrance - $scope.total_cost;
+				});
 
-            });
+				$scope.percentage_cost_income = 0;
+				if ($scope.total_income > 0)
+					$scope.percentage_cost_income = ($scope.total_cost / $scope.total_income) * 100;
 
-        }).
-        error(function(data, status, headers, config) {
-            alert('Opssss!!! the server is not ready');
+				$scope.difference_cost_income = $scope.total_income - $scope.total_cost;
 
-            console.log(data);
-            console.log(status);
-        });
+			}).
+			error(function(data, status, headers, config) {
+				alert('Opssss!!! the server is not ready');
 
-    };
+				console.log(data);
+				console.log(status);
+			});
 
-    $scope.getStatistics();
+		};
 
-}]);
+		$scope.getStatistics();
+
+	}]);
