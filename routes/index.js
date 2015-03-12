34b1056a04f80c2ca4d25f5435496b57340dbc94
Matteo Bovetti <matteobovetti@gmail.com>
2015-03-12 17:51:07 +0100

    coding statistics GET

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var moment = require('moment');
+var _ = require('underscore');
 var router = express.Router();
 
 mongoose.connect('mongodb://localhost:27017/walletsdb');
@@ -16,21 +17,12 @@ var movSchema = mongoose.Schema({
 	    frequency: Number
 });
 
-/* GET home page. */
-/*
-router.get('/', function(req, res, next) {
-	res.render('index');
-});
-*/
-
 router.get('/movements', function(req, res, next) {
 	var Movements = mongoose.model('movements', movSchema);
     
     var fromdt = moment([req.query.y, req.query.m - 1, 1]);
     var todt = moment(fromdt.toDate()).add(1, 'months');
     
-	// TO DO: add "Fixed" frequecy movements filter.
-	
     var query = Movements.find()
     .where('date')
         .gte(fromdt.toDate())
@@ -40,6 +32,57 @@ router.get('/movements', function(req, res, next) {
       if (err) return handleError(err);
         // Ok.
 		res.status(200).json(results);
+    });
+});
+
+router.get('/statistics', function(req, res, next) {
+	var Movements = mongoose.model('movements', movSchema);
+    
+    var fromdt = moment([req.query.y, req.query.m - 1, 1]);
+    var todt = moment(fromdt.toDate()).add(1, 'months');
+    
+	var statistics = {
+		total_cost : 0,
+		total_income : 0,
+		percentage_cost_income : 0,
+		difference_cost_income : 0,
+		total_yearly_cost : 0,
+		total_yearly_income : 0
+	};
+	
+    var query = Movements.find()
+    .where('date')
+        .gte(fromdt.toDate())
+        .lt(todt.toDate())
+    .exec(function (err, results) {
+      if (err) return handleError(err);
+        // Ok.
+		_.each(list, function(value, key, list){
+			
+			if (value.frequencytype === 'f') {
+				if (value.amount > 0)
+					statistics.total_yearly_income += value.amount;
+				else
+					statistics.total_yearly_cost += (-1)*value.amount;
+			}
+			else {
+				if (value.amount > 0)
+					statistics.total_income += value.amount;
+				else
+					statistics.total_cost += (-1)*value.amount;
+			}
+				
+		});
+		
+		statistics.percentage_cost_income = 0;
+		if (statistics.total_income > 0)
+			statistics.percentage_cost_income = (statistics.total_cost / statistics.total_income) * 100;
+
+		statistics.difference_cost_income = statistics.total_income - statistics.total_cost;
+		
+		// Ok.
+		res.status(200).json(statistics);
+		
     });
 });
 
