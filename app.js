1263ff4c1ab841e59039c94202af80e25bb37ea9
Matteo Bovetti <matteobovetti@gmail.com>
2015-05-18 13:22:31 +0200

    deleted user.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/app')));
+app.use(express.static(path.join(__dirname, 'walletsApp/app')));
 
 app.use('/', routes);
 //app.use('/users', users);
